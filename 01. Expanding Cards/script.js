89acb89135627c593c960f4db0469bcf0f2960fe
ephraim <ephraims.818@gmail.com>
2025-09-05 20:13:48 +0600

    JS-PROJECTS--50D50P - EXPANDING-CARDS - 02. Expanding Effects

---
+++ 01. Expanding Cards/script.js
@@ -0,0 +1,15 @@
+"use strict";
+const panels = document.querySelectorAll(".panel");
+
+const removeActiveClass = function () {
+    panels.forEach((panel) => {
+        panel.classList.remove("active");
+    });
+};
+
+panels.forEach((panel) => {
+    panel.addEventListener("click", () => {
+        removeActiveClass();
+        panel.classList.add("active");
+    });
+});
