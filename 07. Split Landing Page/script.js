96fee654afebe09ba29cae6ea9bbf3341af74424
ephraim <ephraims.818@gmail.com>
2025-09-08 19:37:47 +0600

    JS-PROJECTS--50D50P - SPLIT-LANDING-PAGE - 02. Adding The Hover Effect

--- 07. Split Landing Page/script.js
+++ 07. Split Landing Page/script.js
@@ -1,1 +1,19 @@
 "use strict";
+
+const container = document.querySelector(".container");
+const left = document.querySelector(".left");
+const right = document.querySelector(".right");
+
+left.addEventListener("mouseenter", () =>
+    container.classList.add("hover-left")
+);
+left.addEventListener("mouseleave", () =>
+    container.classList.remove("hover-left")
+);
+
+right.addEventListener("mouseenter", () =>
+    container.classList.add("hover-right")
+);
+right.addEventListener("mouseleave", () =>
+    container.classList.remove("hover-right")
+);
