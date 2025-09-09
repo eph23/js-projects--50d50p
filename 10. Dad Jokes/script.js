97a3c0b61574659ef1a70a6abee374faded2d2f3
ephraim <ephraims.818@gmail.com>
2025-09-09 06:29:53 +0600

    JS-PROJECTS--50D50P - DAD-JOKES - 02. Fetching Displaying Jokes

--- 10. Dad Jokes/script.js
+++ 10. Dad Jokes/script.js
@@ -1,1 +1,35 @@
 "use strict";
+
+const jokeEl = document.getElementById("joke");
+const jokeBtn = document.getElementById("joke-btn");
+
+const generateJoke = async function () {
+    const config = {
+        headers: {
+            Accept: "application/json",
+        },
+    };
+
+    const res = await fetch("https://icanhazdadjoke.com/", config);
+
+    const data = await res.json();
+
+    jokeEl.innerHTML = data.joke;
+};
+
+/* const generateJoke = function () {
+    const config = {
+        headers: {
+            Accept: "application/json",
+        },
+    };
+
+    fetch("https://icanhazdadjoke.com/", config)
+        .then((res) => res.json())
+        .then((data) => {
+            jokeEl.innerHTML = data.joke;
+        });
+}; */
+
+generateJoke();
+jokeBtn.addEventListener("click", generateJoke);
