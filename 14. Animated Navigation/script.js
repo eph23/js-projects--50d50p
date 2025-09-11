0f479d2cfb6154b040ac7b7c5add583f06fbc0e1
ephraim <ephraims.818@gmail.com>
2025-09-11 20:39:52 +0600

    JS-PROJECTS--50D50P - ANIMATED-NAVIGATION - 02. Icon Styling Active Toggle

--- 14. Animated Navigation/script.js
+++ 14. Animated Navigation/script.js
@@ -1,1 +1,8 @@
 "use strict";
+
+const toggle = document.getElementById("toggle");
+const nav = document.getElementById("nav");
+
+toggle.addEventListener("click", function () {
+    nav.classList.toggle("active");
+});
