96e45d97226b7b98bd072ff85a28af64595bf244
ephraim <ephraims.818@gmail.com>
2025-09-12 04:53:39 +0600

    JS-PROJECTS--50D50P - DRINK-WATER - 02. Fill Small Cups

--- 16. Drink Water/script.js
+++ 16. Drink Water/script.js
@@ -1,1 +1,29 @@
 "use strict";
+
+const smallCups = document.querySelectorAll(".cup-small");
+const listers = document.getElementById("liters");
+const percentage = document.getElementById("percentage");
+const remained = document.getElementById("remained");
+
+smallCups.forEach((cup, index) => {
+    cup.addEventListener("click", () => {
+        highlightCups(index);
+    });
+});
+
+function highlightCups(index) {
+    if (
+        smallCups[index].classList.contains("full") &&
+        !smallCups[index].nextElementSibling.classList.contains("full")
+    ) {
+        index--;
+    }
+
+    smallCups.forEach((cup, index_2) => {
+        if (index_2 <= index) {
+            cup.classList.add("full");
+        } else {
+            cup.classList.remove("full");
+        }
+    });
+}
