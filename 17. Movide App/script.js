c92efeca89454da27b085e2cf8a28a38c3e608fd
ephraim <ephraims.818@gmail.com>
2025-09-13 21:55:53 +0600

    JS-PROJECTS--50D50P - MOVIE-APP - 03. Adding Movies To the DOM

--- 17. Movide App/script.js
+++ 17. Movide App/script.js
@@ -19,7 +19,45 @@ async function getMovies(url) {
     showMovies(data.results);
 }
 
+function showMovies(movies) {
+    main.innerHTML = "";
+    movies.forEach((movie) => {
+        const { title, poster_path, vote_average, overview } = movie;
 
+        const movieEl = document.createElement("div");
+        movieEl.classList.add("movie");
+        movieEl.innerHTML = `
+                <img
+                    src="${IMG_PATH + poster_path}"
+                    alt="${title}"
+                />
+                <div class="movie-info">
+                    <h3>${title}</h3>
+                    <span class="${getClassByRate(
+                        vote_average
+                    )}">${vote_average.toFixed(1)}</span>
+                </div>
+                <div class="overview">
+                    <h3>Overview</h3>
+                    <p>
+                        ${overview}
+                    </p>
+                </div>
+        `;
+
+        main.appendChild(movieEl);
+    });
+}
+
+function getClassByRate(vote) {
+    if (vote >= 8) {
+        return "green";
+    } else if (vote >= 5) {
+        return "orange";
+    } else {
+        return "red";
+    }
+}
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
