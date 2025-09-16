120cbb9dbf9c46f379c070c882507082536866d3
ephraim <ephraims.818@gmail.com>
2025-09-16 17:11:22 +0600

    JS-PROJECTS--50D50P - DRAWING-APP - 03. Mouse Events Drawing

--- 22. Drawing App/script.js
+++ 22. Drawing App/script.js
@@ -4,8 +4,33 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
 let size = 20;
+let isPressed = false;
 let color = "black";
 let x, y;
+
+canvas.addEventListener("mousedown", (event) => {
+    isPressed = true;
+
+    x = event.offsetX;
+    y = event.offsetY;
+});
+canvas.addEventListener("mouseup", (event) => {
+    isPressed = false;
+
+    x = undefined;
+    y = undefined;
+});
+canvas.addEventListener("mousemove", (event) => {
+    if (isPressed) {
+        const x2 = event.offsetX;
+        const y2 = event.offsetY;
+
+        drawCircle(x2, y2);
+        drawLine(x, y, x2, y2);
+
+        (x = x2), (y = y2);
+    }
+});
 
 function drawCircle(x, y) {
     ctx.beginPath();
@@ -20,7 +45,7 @@ function drawLine(x1, y1, x2, y2) {
     ctx.moveTo(x1, y1);
     ctx.lineTo(x2, y2);
     ctx.strokeStyle = color;
-    ctx.lineWidth = size;
+    ctx.lineWidth = size * 2;
     ctx.stroke();
 }
 
