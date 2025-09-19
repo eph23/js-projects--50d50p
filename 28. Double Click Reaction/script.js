943baf16a2055b74f2addf262d84b4e3e7d34c82
ephraim <ephraims.818@gmail.com>
2025-09-19 07:13:07 +0600

    JS-PROJECTS--50D50P - DOUBLE-CLICK-REACTION - 02. Show Heart Based On Click Position

--- 28. Double Click Reaction/script.js
+++ 28. Double Click Reaction/script.js
@@ -1,1 +1,43 @@
 "use strict";
+
+const loveMe = document.querySelector(".loveMe");
+const times = document.querySelector("#times");
+
+let clickTime = 0;
+let timesClicked = 0;
+
+const createHeart = function (event) {
+    const heart = document.createElement("i");
+    heart.classList.add("fas");
+    heart.classList.add("fa-heart");
+
+    const x = event.clientX;
+    const y = event.clientY;
+
+    const leftOffset = event.target.offsetLeft;
+    const topOffset = event.target.offsetTop;
+
+    const xInside = x - leftOffset;
+    const yInside = y - topOffset;
+
+    heart.style.top = `${yInside}px`;
+    heart.style.left = `${xInside}px`;
+
+    loveMe.appendChild(heart);
+    times.innerHTML = ++timesClicked;
+
+    setTimeout(() => heart.remove(), 1000);
+};
+
+loveMe.addEventListener("click", (event) => {
+    if (clickTime === 0) {
+        clickTime = new Date().getTime();
+    } else {
+        if (new Date().getTime() - clickTime < 800) {
+            createHeart(event);
+            clickTime = 0;
+        } else {
+            clickTime = new Date().getTime();
+        }
+    }
+});
