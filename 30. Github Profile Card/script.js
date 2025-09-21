c1df25d27939c6fc2b88511d68e68dc9253eee76
ephraim <ephraims.818@gmail.com>
2025-09-21 15:01:46 +0600

    JS-PROJECTS--50D50P - GITHUB-PROFILE-CARD - 02. Fetching Profile Data with Axios

--- 30. Github Profile Card/script.js
+++ 30. Github Profile Card/script.js
@@ -1,1 +1,26 @@
 "use strict";
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+
+const APIURL = "https://api.github.com/users/";
+
+async function getUser(userName) {
+    try {
+        const { data } = await axios(APIURL + userName);
+        console.log(data);
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const user = search.value;
+
+    if (user) {
+        getUser(user);
+
+        search.value = "";
+    }
+});
