4d2cdd49be93e6fd8cbd7ee4471ae49bad1ef1d0
ephraim <ephraims.818@gmail.com>
2025-09-25 03:27:34 +0600

    JS-PROJECTS--50D50P - ANIMATED-COUNTDOWN - 03. Dynamic Animations With JS

--- 34. Animated Countdown/script.js
+++ 34. Animated Countdown/script.js
@@ -1,1 +1,45 @@
 "use strict";
+
+const nums = document.querySelectorAll(".nums span");
+const counter = document.querySelector(".counter");
+const finalMessage = document.querySelector(".final");
+const reply = document.querySelector("#reply");
+
+function resetDOM() {
+    counter.classList.remove("hide");
+    finalMessage.classList.remove("show");
+
+    nums.forEach((num) => {
+        num.classList.value = "";
+    });
+
+    nums[0].classList.add("in");
+}
+
+function runAnimation() {
+    nums.forEach((num, index) => {
+        const nextToLast = nums.length - 1;
+
+        num.addEventListener("animationend", function (event) {
+            if (event.animationName === "goIn" && index !== nextToLast) {
+                num.classList.remove("in");
+                num.classList.add("out");
+            } else if (
+                event.animationName === "goOut" &&
+                num.nextElementSibling
+            ) {
+                num.nextElementSibling.classList.add("in");
+            } else {
+                counter.classList.add("hide");
+                finalMessage.classList.add("show");
+            }
+        });
+    });
+}
+
+reply.addEventListener("click", function () {
+    resetDOM();
+    runAnimation();
+});
+
+runAnimation();
