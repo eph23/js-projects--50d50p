b1c0c861b5b66906a8d6c587da6a2a2b3d74d9e1
ephraim <ephraims.818@gmail.com>
2025-11-22 03:04:42 +0600

    JS-PROJECTS--50D50P - PASSWORD-STRENGTH - 02. Change Blur On Input

--- 39. Password Strength/script.js
+++ 39. Password Strength/script.js
@@ -1,1 +1,13 @@
 "use strict";
+
+const password = document.querySelector("#password");
+const background = document.querySelector("#background");
+
+password.addEventListener("input", (event) => {
+    const input = event.target.value;
+    const length = input.length;
+
+    const blurValue = 20 - length * 2;
+
+    background.style.filter = `blur(${blurValue}px)`;
+});
