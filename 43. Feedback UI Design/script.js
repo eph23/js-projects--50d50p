c85842a7d094a9e59b822f5df2104bf51426e49d
ephraim <ephraims.818@gmail.com>
2025-11-26 22:00:04 +0600

    JS-PROJECTS--50D50P - FEEDBACK-UI-DESIGN - 02. Select Submit Feedback

--- 43. Feedback UI Design/script.js
+++ 43. Feedback UI Design/script.js
@@ -1,1 +1,31 @@
 "use strict";
+
+const ratings = document.querySelectorAll(".rating");
+const ratingsContainer = document.querySelector(".ratings-container");
+const sendBtn = document.querySelector("#send");
+const panel = document.querySelector("#panel");
+let selectedRating = "Satisfied";
+
+function removeActive() {
+    ratings.forEach((rating) => rating.classList.remove("active"));
+}
+
+sendBtn.addEventListener("click", (event) => {
+    panel.innerHTML = `
+     <i class="fas fa-heart"></i>
+     <strong>Thank you</strong>
+     <br>
+     <strong>Feedback: ${selectedRating}</strong>
+     <p>We'll use your feedback to improve our customer support</p>
+     `;
+});
+
+ratingsContainer.addEventListener("click", (event) => {
+    if (event.target.parentNode.classList.contains("rating")) {
+        removeActive();
+
+        event.target.parentNode.classList.add("active");
+        selectedRating = event.target.nextElementSibling.innerHTML;
+        console.log(selectedRating);
+    }
+});
