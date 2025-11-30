f117cc046d1e7a8c2828384e52bc5ef45d69d794
ephraim <ephraims.818@gmail.com>
2025-11-30 22:27:39 +0600

    JS-PROJECTS--50D50P - TODO-LIST - 03. Saving Todos to Local Storage

--- 49. Tod List/script.js
+++ 49. Tod List/script.js
@@ -4,6 +4,11 @@ const form = document.getElementById("form");
 const input = document.getElementById("input");
 const todosUL = document.getElementById("todos");
 
+const todos = JSON.parse(localStorage.getItem("todos"));
+if (todos) {
+    todos.forEach((todo) => addTodo(todo));
+}
+
 form.addEventListener("submit", (event) => {
     event.preventDefault();
 
@@ -25,17 +30,36 @@ function addTodo(todo) {
 
         todoEl.innerText = todoText;
 
-        todoEl.addEventListener("click", () =>
-            todoEl.classList.toggle("completed")
-        );
+        todoEl.addEventListener("click", () => {
+            todoEl.classList.toggle("completed");
+            updateLS();
+        });
 
         todoEl.addEventListener("contextmenu", (event) => {
             event.preventDefault();
             todoEl.remove();
+            updateLS();
         });
 
         todosUL.appendChild(todoEl);
 
         input.value = "";
+
+        updateLS();
     }
+}
+
+function updateLS() {
+    let todosEl = document.querySelectorAll("li");
+
+    const todos = [];
+
+    todosEl.forEach((todoEl) => {
+        todos.push({
+            text: todoEl.innerText,
+            completed: todoEl.classList.contains("completed"),
+        });
+    });
+
+    localStorage.setItem("todos", JSON.stringify(todos));
 }
