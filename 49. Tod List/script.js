d99445cb920abb856403f8d68780bc3428b86036
ephraim <ephraims.818@gmail.com>
2025-11-30 22:07:42 +0600

    JS-PROJECTS--50D50P - TODO-LIST - 02. Add Remove and Mark Completed Functionality

--- 49. Tod List/script.js
+++ 49. Tod List/script.js
@@ -1,1 +1,41 @@
 "use strict";
+
+const form = document.getElementById("form");
+const input = document.getElementById("input");
+const todosUL = document.getElementById("todos");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    addTodo();
+});
+
+function addTodo(todo) {
+    let todoText = input.value;
+
+    if (todo) {
+        todoText = todo.text;
+    }
+
+    if (todoText) {
+        const todoEl = document.createElement("li");
+        if (todo && todo.completed) {
+            todoEl.classList.add("completed");
+        }
+
+        todoEl.innerText = todoText;
+
+        todoEl.addEventListener("click", () =>
+            todoEl.classList.toggle("completed")
+        );
+
+        todoEl.addEventListener("contextmenu", (event) => {
+            event.preventDefault();
+            todoEl.remove();
+        });
+
+        todosUL.appendChild(todoEl);
+
+        input.value = "";
+    }
+}
